caefc329911f68e6379bb92c1652db795be1e3b1
amarj <abg@gmail.com>
2025-01-22 20:57:07 +1100

    Add CORS configuration to SecurityConfig
    Enabled CORS support by introducing a configuration source that defines allowed origins, methods, and headers. This ensures better compatibility with frontend applications, particularly for local development scenarios. Updated the SecurityFilterChain to use the newly added CORS configuration.

--- security_management/src/main/java/dev/aj/security_management/security/config/SecurityConfig.java
+++ security_management/src/main/java/dev/aj/security_management/security/config/SecurityConfig.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -27,23 +29,20 @@ public class SecurityConfig {
     /**
      * Configures the SecurityFilterChain for HTTP security.
      *
-     *<p>
+     * <p>
      * <a href="https://docs.spring.io/spring-security/site/docs/5.5.4/guides/form-javaconfig.html">Spring Security Setup</a>
-     *</p>
+     * </p>
      *
-     *  <p>This method defines access rules for various HTTP methods and endpoints.
+     * <p>This method defines access rules for various HTTP methods and endpoints.
      * The {@code requestMatchers} method is used to specify the URL patterns,
      * and it is noteworthy that the servlet context-path is automatically excluded
      * when matching the request URLs. For example, if the application's context-path
      * is "/api", a request to "/api/employee/123" matches the pattern "/employee/**"
      * without needing to include "/api" in the pattern.</p>
      *
-     *
      * @param http the {@link HttpSecurity} to modify
      * @return the configured {@link SecurityFilterChain}
      * @throws Exception if there is an issue in configuration
-     *
-
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -59,6 +58,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/favicon.ico").permitAll()
                         .anyRequest().denyAll()
                 )
+                .cors(configurer ->
+                        configurer.configurationSource(configurationSource()))
                 .httpBasic(Customizer.withDefaults())
                 .csrf(Customizer.withDefaults())
                 .formLogin(form -> form
@@ -68,15 +69,30 @@ public class SecurityConfig {
                         .successForwardUrl("/security/home")
                 )
                 .logout(logoutConfigurer ->
-                        logoutConfigurer
-                                .logoutUrl("/security/logout")
+                                logoutConfigurer
+                                        .logoutUrl("/security/logout")
 //                                .logoutSuccessUrl("/security/login")
-                                .permitAll()
+                                        .permitAll()
                 )
                 .exceptionHandling(configurer -> configurer
                         .accessDeniedPage("/security/access-denied")
                 )
                 .build();
+    }
+
+    private CorsConfigurationSource configurationSource() {
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3000L);
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setAllowedMethods(List.of(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return corsConfigurationSource;
     }
 
     @Bean
