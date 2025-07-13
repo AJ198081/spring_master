ca5ec78637ce33cbc4784f95970450a7e5be58f3
agb <abhandal@umd.edu>
2025-07-13 16:47:40 +1000

    Refactor and enhance frontend data handling, improve backend services, and introduce new features
    - Added `CartQuantityUpdater` and `ImageZoom` components for improved user interaction.
    - Refined `Sidebar`, `ProductStore`, `SearchBar`, and `Products` components for dynamic product filtering and state management.
    - Introduced new services (`CartService`, `ImageService`) to streamline API communication.
    - Enhanced backend services: added `getAllCustomers` and improved logic for `CartItem` updates and total calculations.
    - Updated mappers, DTOs, and tests to align with new functionalities and maintain consistency.

---
+++ full_stack_v5/src/main/frontend/src/components/product/ProductDetails.tsx
@@ -0,0 +1,157 @@
+import {ProductImage} from "../ProductImage.tsx";
+import {type Product, useProductStore} from "../../store/ProductStore.tsx";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getProductById} from "../../services/ProductService.ts";
+import {CartQuantityUpdater} from "./CartQuantityUpdater.tsx";
+import {BsCart} from "react-icons/bs";
+import {toast, ToastContainer} from "react-toastify";
+import {type AddCartItem, addProductToCartItems} from "../../services/CartService.ts";
+
+export const ProductDetails = () => {
+
+    const {productId} = useParams();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [quantity, setQuantity] = useState(0);
+    const allAvailableProducts = useProductStore(state => state.allProducts);
+    const updateAllProducts = useProductStore(state => state.setAllProducts);
+
+    useEffect(() => {
+        if (productId && productId.length > 0) {
+            getProductById(Number(productId))
+                .then((product) => {
+                    if (product) {
+                        setProduct(product);
+                    }
+                })
+                .catch((error) => {
+                        console.log(error);
+                        setProduct({} as Product);
+                    }
+                )
+        }
+    }, [productId, allAvailableProducts])
+
+    const optionalInventory = allAvailableProducts.filter(product => product.id === Number(productId))[0]?.inventory;
+    const inventory = optionalInventory || 0;
+
+    const addProductToCart = () => {
+
+        if (inventory <= 0) {
+            return;
+        }
+
+        if (quantity > inventory) {
+            return;
+        }
+
+        updateAllProducts(allAvailableProducts
+            .map(product => {
+                if (product.id === Number(productId)) {
+                    console.log(product.id, quantity, product.inventory);
+                    return {
+                        ...product,
+                        inventory: product.inventory - quantity
+                    }
+                }
+                return product;
+            }));
+
+        const cartItemRequest : AddCartItem = {
+            productId: Number(productId),
+            quantity: quantity
+        }
+
+        addProductToCartItems(cartItemRequest)
+            .then(() => {
+                toast.success(`Product ${product?.name} added to cart successfully`);
+            })
+            .catch((error) => {
+                toast.error(`Error adding product to cart; issue is - ${error.response?.data?.detail}`);
+            })
+            .finally(() => {
+                    setQuantity(0);
+                }
+            );
+
+    };
+    return (
+        product && <div className={'container'}>
+            <div className="row product-details">
+                <ToastContainer
+                    position={"bottom-right"}
+                    autoClose={5000}
+                    newestOnTop={false}
+                    closeOnClick
+                    pauseOnFocusLoss
+                    pauseOnHover
+                />
+                <div className="col-md-2">
+                    {
+                        product.images.map((image) => (
+                            <div
+                                key={image.downloadUrl}
+                                className={"mt-4 image-container"}
+                            >
+                                <ProductImage imageDownloadUrl={image.downloadUrl}/>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="col-md-8 details-container">
+                    <h1 className={'product-name'}>{product.name}</h1>
+                    <h4 className={'price'}>{product.price}</h4>
+                    <p className={'product-description'}>{product.description}</p>
+                    <p className="product-name">Brand: {product.brand.toLowerCase()}</p>
+                    <p className="product-name">
+                        Rating: <span className="rating text-danger">4.5</span>
+                    </p>
+                    <p className={`${inventory >= 5 ? 'text-success' : 'text-danger'}`}>
+                        {inventory >= 5
+                            ? 'In stock'
+                            : inventory === 0
+                                ? 'Out of stock'
+                                : 'Low stock'}
+                    </p>
+                    <p>Quantity: {inventory}</p>
+                    <CartQuantityUpdater
+                        quantity={quantity}
+                        setQuantity={setQuantity}
+                        maxQuantity={inventory}
+                        onDecrease={() => setQuantity(prevState => {
+                            return prevState === 0 ? 0 : prevState - 1
+                        })}
+                        onIncrease={() => setQuantity(prevState => {
+                            if (inventory === 0) {
+                                // alert('Out of stock');
+                                toast.error('Out of stock');
+                                return prevState;
+                            }
+                            if (prevState === inventory) {
+                                // alert('Max quantity reached');
+                                toast.error('Max quantity reached')
+                                return prevState;
+                            }
+
+                            return prevState === product?.inventory ? prevState : prevState + 1;
+                        })
+                        }
+                    />
+
+                    <div className="d-flex gap-2 mt-3">
+                        <button
+                            className={"add-to-cart-button" + (inventory === 0 ? ' text-bg-dark' : '')}
+                            onClick={addProductToCart}
+                            disabled={inventory === 0}
+                        >
+                           <BsCart /> Add to cart
+                        </button>
+                        <button className={"buy-now-button" + (inventory === 0 ? ' text-bg-dark' : '')}>
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
