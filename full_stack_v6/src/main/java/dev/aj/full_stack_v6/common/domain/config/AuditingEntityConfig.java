5b07780be623ac7330d7d4e6653935b5ef22fcd3
agb <abhandal@umd.edu>
2025-09-21 05:26:11 +1000

    Enhance `AuditingEntityConfig` with documentation and `proxyBeanMethods` optimization
    - Added Javadoc comments for better documentation of beans (`auditorProvider` and `dateTimeProvider`).
    - Disabled proxying by setting `proxyBeanMethods` to `false` for improved performance and accuracy in bean definitions.

--- full_stack_v6/src/main/java/dev/aj/full_stack_v6/common/domain/config/AuditingEntityConfig.java
+++ full_stack_v6/src/main/java/dev/aj/full_stack_v6/common/domain/config/AuditingEntityConfig.java
@@ -7,9 +7,19 @@ import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
 
-@Configuration
+/**
+ * @Configuration - disabling proxying for the beans defined in this class.
+ * Static beans that are called every time, without Spring proxying them,
+ * especially for the auditorProvider, which needs to look at the SecurityContextHolder for each thread.
+ */
+@Configuration(proxyBeanMethods = false)
 public class AuditingEntityConfig {
 
+    /**
+     * Provides the current auditor for entity auditing.
+     *
+     * @return AuditorAware instance that supplies the current user identifier
+     */
     @Bean
     public static AuditorAware<String> auditorProvider() {
         return () -> {
@@ -21,6 +31,11 @@ public class AuditingEntityConfig {
         };
     }
 
+    /**
+     * Provides the current date-time for entity auditing.
+     *
+     * @return DateTimeProvider instance that supplies the current ZonedDateTime
+     */
     @Bean
     public static DateTimeProvider dateTimeProvider() {
         return () -> Optional.of(java.time.ZonedDateTime.now());
