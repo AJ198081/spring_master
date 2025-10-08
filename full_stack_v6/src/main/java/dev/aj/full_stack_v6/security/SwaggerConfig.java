1c74fec5d71330fb8e9a96b699f2261c9e4ae2c6
agb <abhandal@umd.edu>
2025-10-09 08:09:38 +1100

    Enhance Swagger configuration with API details and improve `AuthController` documentation
    - Updated `SwaggerConfig` to include API tags, descriptions, and metadata with detailed spec.
    - Added OpenAPI annotations to `AuthController` for better documentation.
    - Introduced `OrderApplicationSpecs.json` to define API specifications.

--- full_stack_v6/src/main/java/dev/aj/full_stack_v6/security/SwaggerConfig.java
+++ full_stack_v6/src/main/java/dev/aj/full_stack_v6/security/SwaggerConfig.java
@@ -2,10 +2,14 @@ package dev.aj.full_stack_v6.security;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,14 +20,43 @@ public class SwaggerConfig {
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT")
-                .description("Add JWT for token-based authentication");
+                .description("Add JWT for token-based authentica" +
+                        "tion");
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList("bearer-jwt");
 
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
+                .info(new Info()
+                        .title("Orders APIs")
+                        .version("v1.0.0")
+                        .summary("Publicly exposed APIs for the Ordering Application")
+                        .description("""
+                                RESTful APIs for managing customers, vendors, carts, payments, and orders operations in the Ordering Application.
+                                These endpoints allow customers to browse products, manage shopping carts, place orders, and process payments.
+                                Includes secure authentication and authorization mechanisms using JWT tokens.
+                               """)
+                )
+                .tags(List.of(
+                                new Tag().name("Checkout")
+                                        .description("Checkout and order management related operations"),
+                                new Tag().name("AuthN")
+                                        .description("Authentication and authorization-related operations"),
+                                new Tag().name("Cart")
+                                        .description("Cart-management related operations"),
+                                new Tag().name("Product")
+                                        .description("Product-management operations"),
+                                new Tag().name("Customer")
+                                        .description("Customer-management operations"),
+                                new Tag().name("Vendor")
+                                        .description("Vendor-management operations"),
+                                new Tag().name("Payment")
+                                        .description("Payment-management operations")
+                        )
+                )
                 .components(new Components()
                         .addSecuritySchemes("bearer-jwt", securityScheme));
     }
+
 }
--- full_stack_v6/src/main/java/dev/aj/full_stack_v6/security/controllers/AuthController.java
+++ full_stack_v6/src/main/java/dev/aj/full_stack_v6/security/controllers/AuthController.java
@@ -3,6 +3,7 @@ package dev.aj.full_stack_v6.security.controllers;
 import dev.aj.full_stack_v6.common.domain.dtos.LoginRequest;
 import dev.aj.full_stack_v6.common.domain.dtos.LoginResponse;
 import dev.aj.full_stack_v6.security.services.impl.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -21,6 +22,7 @@ class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @Operation(summary = "Login", description = "Login to the application", tags = {"AuthN"})
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginDto, HttpServletResponse response) {
         return ResponseEntity.ok(authService.login(loginDto, response));
     }
