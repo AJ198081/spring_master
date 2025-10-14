622142302d61cb206ed255dca6f3a9519f0a5a8b
agb <abhandal@umd.edu>
2025-10-15 03:56:30 +1100

    Initialize `full_stack_v6_kafka` module with Kafka setup and Spring Boot integration
    - Added base project structure with `FullStackV6KafkaApplication` and test class.
    - Included essential Kafka and test dependencies in `pom.xml`.
    - Configured multi-broker Kafka setup with Docker Compose.
    - Added `.env` file for externalizing environment variables.
    - Updated database and mail configurations in Docker Compose.

---
+++ full_stack_v6_kafka/src/main/java/dev/aj/full_stack_v6_kafka/FullStackV6KafkaApplication.java
@@ -0,0 +1,13 @@
+package dev.aj.full_stack_v6_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackV6KafkaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FullStackV6KafkaApplication.class, args);
+    }
+
+}
---
+++ full_stack_v6_kafka/src/test/java/dev/aj/full_stack_v6_kafka/FullStackV6KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aj.full_stack_v6_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackV6KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
